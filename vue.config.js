f82598ea8048f3aef15495b866471ede70c1895e
joaosilvabefore <92741701+joaosilvabefore@users.noreply.github.com>
2024-08-20 12:43:52 -0300

    initial commit

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require('@vue/cli-service');
+module.exports = defineConfig({
+  transpileDependencies: true,
+  configureWebpack: {
+    output: {
+      libraryTarget: 'system',
+    },
+    devServer: {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      allowedHosts: 'all',
+      https: true,
+    },
+  },
+});
